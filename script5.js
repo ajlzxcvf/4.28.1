3885d6eadc4f495bd53fec3e22b88325cc1de56c
Chris <102649652+ajlzxcvf@users.noreply.github.com>
2022-05-03 19:41:50 +0800

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,6 @@
+function myFunction1() {
+  location.replace("https://ajlzxcvf.github.io/4.28.1/")
+}
+function myFunction() {
+  location.replace("https://ajlzxcvf.github.io/4.28.1/index4.html")
+}
